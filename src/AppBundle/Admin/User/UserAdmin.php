8feffe333e765497731e81d109e02fab97f58735
developer <zastrevok.kosiak@gmail.com>
2018-04-14 16:18:50 +0300

    14.04 админ для юзеров. парольне хешируется, надо исправить

---
+++ src/AppBundle/Admin/User/UserAdmin.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: developer
+ * Date: 4/14/18
+ * Time: 3:24 PM
+ */
+
+namespace AppBundle\Admin\User;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class UserAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('firstName');
+        $formMapper->add('middleName');
+        $formMapper->add('lastName');
+        $formMapper->add('username');
+        $formMapper->add('email');
+        $formMapper->add('password');
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('firstName');
+        $datagridMapper->add('middleName');
+        $datagridMapper->add('lastName');
+        $datagridMapper->add('username');
+        $datagridMapper->add('email');
+        $datagridMapper->add('password');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->add('firstName');
+        $listMapper->add('middleName');
+        $listMapper->add('lastName');
+        $listMapper->add('username');
+        $listMapper->add('email');
+        $listMapper->add('password');
+    }
+}
