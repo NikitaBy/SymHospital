cc27772dc7911cb821782262ad59c50a95c7f2d7
developer <zastrevok.kosiak@gmail.com>
2018-04-27 14:03:31 +0300

    27.04 доделываю форму регистрации

--- src/AppBundle/Form/Type/RegistrationType.php
+++ src/AppBundle/Form/Type/RegistrationType.php
@@ -2,10 +2,13 @@
 
 namespace AppBundle\Form\Type;
 
+use AppBundle\Entity\Specialty;
 use AppBundle\Entity\Users\Role;
 use FOS\UserBundle\Form\Type\RegistrationFormType;
+use Sonata\CoreBundle\Form\Type\DatePickerType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class RegistrationType extends AbstractType
@@ -23,6 +26,18 @@ class RegistrationType extends AbstractType
                 'mapped'=>false
             ]
         );
+        $builder->add('birthDate', DatePickerType::class,[
+//            'widget'=>'single_text',
+            'html5'=>false,
+//            'format'=>'yyyy-MMM-dd',
+            'mapped'=>false,
+            'attr'=> ['class'=>'datepicker']
+        ]);
+        $builder->add('specialty', EntityType::class,[
+            'class'=>Specialty::class,
+            'multiple'=>true,
+            'mapped'=>false
+        ]);
     }
 
     public function getParent()
